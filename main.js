b2101ee9249b995b744073ffe3a1345ab452b8ff
Rebecca Martin <rebecca-martin@live.com>
2018-07-24 11:09:05 -0700

    add googlemap function, html, css

--- main.js
+++ main.js
@@ -7,7 +7,10 @@
 area for globals
 venueSearchResults = [];
 */
-
+var map;
+var service;
+var latitude = 33.69
+var longitude = -117.83
 /*initialize app function
 *call addClickHandlers function
 *no params or returns
@@ -62,6 +65,81 @@ venueSearchResults = [];
 *run google ajax call and populate data onto page4
 *create links on the dropped markers near venue to viewYelpInfo with param of business selected
 */
+
+/* initializeMap
+*params lat and long
+*takes in parameters from search for restaurants / bars / hotels to change type in var request to match what type of place person is searching for
+*/
+
+function initializeMap() {
+    //defines location we are targeting on the map
+    var location = new google.maps.LatLng(latitude, longitude);
+    //creates instance of map
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: location,
+        zoom: 15
+    });
+    //request contains the radius around given location and the type of facility we are targeting
+    var request = {
+        location: location,
+        radius: '500',
+        type: ['restaurant']
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    if (place.icon) {
+        var image = new google.maps.MarkerImage(
+            place.icon, new google.maps.Size(71, 71),
+            new google.maps.Point(0, 0), new google.maps.Point(17, 34),
+            new google.maps.Size(25, 25));
+    } else var image = null;
+
+    var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        position: place.geometry.location
+    });
+    var request = {
+        reference: place.reference
+    };
+
+    var infowindow = new google.maps.InfoWindow({
+        contentStr: ""
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        service.getDetails(request, function (place, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                var contentStr = '<h5>' + place.name + '</h5><p>' + place.formatted_address;
+                if (!!place.formatted_phone_number) contentStr += '<br>' + place.formatted_phone_number;
+                if (!!place.website) contentStr += '<br><a target="_blank" href="' + place.website +
+                    '">' + place.website + '</a>';
+                contentStr += '<br>' + '</p>';
+                infowindow.setContent(contentStr);
+                infowindow.open(map, marker);
+            } else {
+                var contentStr = "<h5>No Result, status=" + status + "</h5>";
+                infowindow.setContent(contentStr);
+                infowindow.open(map, marker);
+            }
+        });
+    });
+}
+
 
 /*viewYelpInfo function
 *params businessSelected
